ee01acaebc2825f293d6717db6603bd64fc746e4
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-12-06 16:42:35 -0600

    working logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,4 @@
 // LOAD DATA
-
-
 const friendData = require("../data/friends");
 
 
@@ -13,10 +11,33 @@ module.exports = (app) => {
     });
 
     // API POST Requests
-
     app.post("/api/friends", (req, res) => {
+        let userScore = req.body.scores;
+        const scoresArr = [];
+        const friendCount = 0;
+        let bestMatch = 0;
+
+        for (var i = 0; i < friendData.length; i++) {
+            var scoreDiff = 0;
+
+            for (var j = 0; j < userScore.length; j++) {
+                scoreDiff += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(userScore[j])))
+            }
+            scoresArr.push(scoreDiff);
+        }
+        for (var i = 0; i < scoresArr.length; i++) {
+            if (scoresArr[i] <= scoresArr[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        // return the best macth
+        let soulMate = friendData[bestMatch];
+        res.json(soulMate);
+
         console.log(req.body);
-        friendData.push(req.body);
+        friendData.push(req.body)
+
     });
 
 
